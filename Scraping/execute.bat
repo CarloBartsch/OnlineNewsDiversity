9da94d24ba5f01a52ccea3a699656d66855d1ac5
Carbinowitz <55796391+Carbinowitz@users.noreply.github.com>
2021-05-21 13:04:53 +0200

    Add files via upload

---
+++ Scraping/execute.bat
@@ -0,0 +1,9 @@
+C:\Users\admin\AppData\Local\Programs\Python\Python37\python.exe "C:\Users\admin\Documents\Dissertation\Diversity of News\Skripte\Final\Spiegel.py"
+C:\Users\admin\AppData\Local\Programs\Python\Python37\python.exe "C:\Users\admin\Documents\Dissertation\Diversity of News\Skripte\Final\focus.py"
+C:\Users\admin\AppData\Local\Programs\Python\Python37\python.exe "C:\Users\admin\Documents\Dissertation\Diversity of News\Skripte\Final\bild.py"
+C:\Users\admin\AppData\Local\Programs\Python\Python37\python.exe "C:\Users\admin\Documents\Dissertation\Diversity of News\Skripte\Final\dlf.py"
+C:\Users\admin\AppData\Local\Programs\Python\Python37\python.exe "C:\Users\admin\Documents\Dissertation\Diversity of News\Skripte\Final\dw.py"
+C:\Users\admin\AppData\Local\Programs\Python\Python37\python.exe "C:\Users\admin\Documents\Dissertation\Diversity of News\Skripte\Final\heute.py"
+C:\Users\admin\AppData\Local\Programs\Python\Python37\python.exe "C:\Users\admin\Documents\Dissertation\Diversity of News\Skripte\Final\tagesschau.py"
+C:\Users\admin\AppData\Local\Programs\Python\Python37\python.exe "C:\Users\admin\Documents\Dissertation\Diversity of News\Skripte\Final\t-online.py"
+pause
