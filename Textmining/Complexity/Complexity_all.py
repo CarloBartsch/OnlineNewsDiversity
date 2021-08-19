a42a8f2c9eef50e991bd6b6321149998f42f3dd9
Carbinowitz <55796391+Carbinowitz@users.noreply.github.com>
2021-08-19 13:54:17 +0200

    Add files via upload

---
+++ Textmining/Complexity/Complexity_all.py
@@ -0,0 +1,48 @@
+#Source: https://towardsdatascience.com/using-data-science-skills-now-text-readability-analysis-c4c4641f5875
+#Source 2: count_words Script
+
+#pip istall textstat
+#pip install readability
+
+#Import
+import textstat
+import os, csv, re
+import glob
+import docx2txt
+import textract
+import pandas as pd
+textstat.set_lang('de')
+
+#Path and Folders(zdf_nachrichten anspassen!!!)
+#folder =['bild','br','dlf','dw','faz','focus','zdf_nachrichten','mdr','ndr','ntv','pro7','rtl','spiegel','stern','swr','sz','tagesschau','taz','tonline','wdr','welt','zeit']
+folder =['bild','dlf','dw','focus','zdf_nachrichten','spiegel','t-online']
+with open('C:/Users/admin/Documents/Dissertation/Diversity of News/Textmining/Complexity/all_complexity.csv','w',encoding='utf8',errors='ignore', newline='') as word_file:
+        word_writer = csv.writer(word_file,delimiter=',')
+
+        word_writer.writerow(['Name','Article','Flesch Reading Ease'])
+        for items in folder:
+            print(items)
+
+
+            entries = os.listdir('C:/Users/admin/Documents/Dissertation/Diversity of News/Files/'+items+'/Clean')
+
+
+
+
+
+
+
+
+            for line in entries:
+
+                    path = 'C:/Users/admin/Documents/Dissertation/Diversity of News/Files/'+items+'/Clean/'+ line
+                    print(path)
+                    try:
+                        text = str(textract.process(path),encoding='utf8',errors='ignore')
+                        Flesch = str(textstat.flesch_reading_ease(text))
+                        print('Flesch Reading Ease:' + str(Flesch))
+
+                        word_writer.writerow([items,line,Flesch])
+                    except Exception as error:
+                        print(error)
+
---
+++ Textmining/Complexity/heute_kinder.py
@@ -0,0 +1,151 @@
+###IMPORT
+
+import requests, re, csv, os
+from bs4 import BeautifulSoup
+from datetime import date
+import locale
+import threading
+#import urllib2
+from csv import writer
+
+from datetime import datetime
+from contextlib import contextmanager
+import datetime
+
+
+from bs4.element import Comment
+import urllib.request
+
+#Append CSV
+def append_list_as_row(file_name, list_of_elem):
+    # Open file in append mode
+    with open(file_name, 'a+', newline='') as write_obj:
+        # Create a writer object from csv module
+        csv_writer = writer(write_obj)
+        # Add contents of list as last row in the csv file
+        csv_writer.writerow(list_of_elem)
+
+
+
+
+def tag_visible(element):
+    if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
+        return False
+    if isinstance(element, Comment):
+        return False
+    return True
+
+def text_from_html(body):
+    soup = BeautifulSoup(body, 'html.parser')
+    texts = soup.findAll(text=True)
+    visible_texts = filter(tag_visible, texts)
+    return u" ".join(t.strip() for t in visible_texts)
+### DATE
+
+@contextmanager
+def setlocale(name):
+    with LOCALE_LOCK:
+        saved = locale.setlocale(locale.LC_ALL)
+        try:
+            yield locale.setlocale(locale.LC_ALL, name)
+        finally:
+            locale.setlocale(locale.LC_ALL, saved)
+
+def replaceMultiple(mainString, toBeReplaces, newString):
+                    # Iterate over the strings to be replaced
+                        for elem in toBeReplaces :
+                    # Check if string is in the main string
+                            if elem in mainString :
+                    # Replace the string
+                                mainString = mainString.replace(elem, newString)
+
+                        return  mainString
+
+# Let's set a non-US locale
+locale.setlocale(locale.LC_ALL, 'de_DE.UTF-8')
+
+today = date.today()
+
+print("Today's date:", today)
+
+d2 = today.strftime("%e. %B %Y")
+#print("d2 =", d2)
+print(str(d2))
+
+#HTML
+
+
+
+
+
+
+website = requests.get('https://www.zdf.de/kinder/logo').text
+html = BeautifulSoup(website,'lxml')
+body = html.find('body')
+        #tagesschau.de\ts_linklist.csv'print(body)
+
+
+try:
+            data=[]
+            with open('C:/Users/admin/Documents/Dissertation/Diversity of News/Helpfiles Skript/zdf_kinder/zdf_kinder_linklist.csv',encoding='utf-8') as file:
+                        reader = csv.reader(f, delimiter=',')
+                        for row in reader:
+                            data.append(row)
+
+except:
+            with open('C:/Users/admin/Documents/Dissertation/Diversity of News/Helpfiles Skript/zdf_kinder/zdf_kinder_linklist.csv','w', newline='',encoding='utf-8') as file:
+                writer = csv.writer(file,delimiter=',')
+                writer.writerow(['Headline','Date','Link'])
+            data=[]
+
+print(data)
+href_column = [x[2] for x in data]
+for link in body.find_all('a'):
+
+                    for div in link.find_all("span",{"class":"normal-space"}):
+                        href = 'https://www.zdf.de' + link.get('href')
+                        print(href)
+                        #reg_1 = re.compile('.+<div')
+                        #name = reg_1.findall(str(div))
+                        name = div.text
+                        name = name.strip()
+                        name = os.linesep.join([s for s in name.splitlines() if s])
+                        name = replaceMultiple(name,["'",'[',']','.',':','?','!','"','/',',','.'],"")
+                        print(name)
+                        row_contents =[name,d2,href]
+                        row_contents_error =['error',d2,'error']
+                        if href in href_column:
+                            print('old')
+
+                        else:
+                            print('new')
+                        #zu Liste hinzufügen
+                            try:
+                                with open(r'C:/Users/admin/Documents/Dissertation/Diversity of News/Helpfiles Skript/zdf_kinder/zdf_kinder_linklist.csv','a',newline='',encoding="utf-8") as f:
+                                    writer = csv.writer(f)
+                                    writer.writerow(row_contents)
+                            except:
+                                with open(r'C:/Users/admin/Documents/Dissertation/Diversity of News/Helpfiles Skript/zdf_kinder/zdf_kinder_linklist.csv','a',newline='',encoding="utf-8") as f:
+                                    writer = csv.writer(f)
+                                    writer.writerow(row_contents_error)
+                            try:
+                                html = urllib.request.urlopen(href).read()
+                                print(text_from_html(html))
+                                article = text_from_html(html)
+                                with open('C:/Users/admin/Documents/Dissertation/Diversity of News/Files/zdf_kinder/%s.txt' % name,'w', encoding="utf-8") as e:
+                                    e.write(article)
+
+
+                            except:
+                                try:
+                                    with open(r'C:/Users/admin/Documents/Dissertation/Diversity of News/Helpfiles Skript/zdf_kinder/zdf_kinder_error.csv','a',newline='',encoding="utf-8") as file_error:
+                                        writer_error = csv.writer(file_error)
+                                        writer_error.writerow(name,d2,href,'no')
+                                except:
+                                    with open('C:/Users/admin/Documents/Dissertation/Diversity of News/Helpfiles Skript/zdf_kinder/zdf_kinder_error.csv','w', newline='',encoding='utf-8') as file_error:
+                                        writer_error = csv.writer(file_error,delimiter=',')
+                                        writer_error.writerow(['Headline','Date','Link','Download'])
+                                        writer_error.writerow([name,d2,href,'no'])
+                                continue
+
+
